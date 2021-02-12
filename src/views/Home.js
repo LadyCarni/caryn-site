d97f755f03a089242ddfe2abca96d2adad792869
Caryn Farvour <caryn.farvour@gmail.com>
2021-02-12 12:14:07 -0500

    adds fontawesome pro

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowDown, faLongArrowRight } from '@fortawesome/pro-light-svg-icons'
 import triangles from '../assets/tri-large.svg'; 
 
 class Home extends Component {
@@ -11,6 +13,7 @@ class Home extends Component {
         <div className="content">
           <h1><span className="pop">Design</span> for work,<br/>life, and play</h1>
         </div>
+        <FontAwesomeIcon icon={faLongArrowDown} className="scroll" alt="scroll down" />
       </div>
       <div className="about feature feature-width feature-height">
         <div className="feature-content">
@@ -21,7 +24,7 @@ class Home extends Component {
             <h3>I <span className="pop">design</span> experiences</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cupiditate corporis sed ea, nobis fugiat facere maiores praesentium perspiciatis tempora id aut culpa obcaecati saepe ipsam voluptatem repudiandae tenetur accusamus!</p>
             <p>
-              <Link to="/about" className="arrow-link">More about Caryn</Link>
+              <Link to="/about" className="arrow-link">More about Caryn <FontAwesomeIcon icon={faLongArrowRight} /></Link>
             </p>
           </div>
         </div>
