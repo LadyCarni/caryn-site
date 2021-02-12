dc35e5566a35de7ffccf11f3c940bf71df3bb8b4
Caryn Farvour <caryn.farvour@gmail.com>
2021-02-12 10:17:16 -0500

    initial home page structure with routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './views/Home';
 
-function App() {
+export default function Site() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="header row feature-width">
+        <span className="brand"><Link to="/">Caryn Farvour</Link></span>
+        <div className="nav">
+          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/articles">Articles</Link>
+          <Link to="/about">About</Link>
+          <Link to="/creative">Creative</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/articles">
+          <Articles />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/creative">
+          <Creative />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Portfolio() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
     </div>
   );
 }
 
-export default App;
+function Articles() {
+  return (
+    <div>
+      <h2>Articles</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Creative() {
+  return (
+    <div>
+      <h2>Creative</h2>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+    </div>
+  );
+}
