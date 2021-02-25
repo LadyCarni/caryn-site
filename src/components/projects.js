3168026a582c953cde11fa4b6ab7cd454166217f
Caryn Farvour <caryn.farvour@gmail.com>
2021-02-25 08:40:47 -0500

    adds test route for project page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Switch, useRouteMatch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, useRouteMatch } from "react-router-dom";
 const Projects = ({ match }) => {
   const gallery = [
     { 
@@ -7,77 +7,77 @@ const Projects = ({ match }) => {
       title: "Planning Project",
       slug: "wow-project",
       category: "Research & Strategy",
-      id: 1,
+      id: 0,
     },
     { 
       source: "https://source.unsplash.com/random?forest",
       title: "So UXY",
       slug: "such-project",
       category: "UX Design",
-      id: 2,
+      id: 1,
     },
     { 
       source: "https://source.unsplash.com/random?work",
       title: "Sites of web",
       slug: "dat-project-tho",
       category: "Website",
-      id: 3
+      id: 2
     },
     { 
       source: "https://source.unsplash.com/random?cafe",
       title: "So research right now",
       slug: "research-project",
       category: "Research & Strategy",
-      id: 4
+      id: 3
     },
     { 
       source: "https://source.unsplash.com/random?phone",
       title: "All the UX",
       slug: "ux-all-the-things",
       category: "UX Design",
-      id: 5
+      id: 4
     },
     { 
       source: "https://source.unsplash.com/random?happy",
       title: "Internets of Something",
       slug: "project-of-projects",
       category: "Website",
-      id: 6
+      id: 5
     },
   ];
 
   const { url, path } = useRouteMatch();
 
-  const projectCards = gallery.map((project) => {
-    return (
-      <div className="column" key={project.id}>
-        <Link to={`${url}/${project.slug}`}>
-          <div className="image filter bottom">
-            <img src={project.source} alt={project.title}/>
-          </div>
-          <div className="caption overlay">
-            <h3>{project.title}</h3>
-            <span>{project.category}</span>
-          </div>
-        </Link>
-      </div>
-    );
-  });
-
   return (
     <div className="light feature feature-width full-height">
-      <Switch>
-        <Route path={`${path}`} exact>
-          <div className="feature-content shadow columns portfolio">
-            {projectCards}
-          </div>
-        </Route>
+      <Router>
+        <Route
+          exact
+          path={`${path}`}
+          render={() => (
+            <div className="feature-content shadow columns portfolio">
+              {gallery.map(project =>(
+                <div className="column" key={project.id}>
+                  <Link to={`${url}/${project.slug}`}>
+                    <div className="image filter bottom">
+                      <img src={project.source} alt={project.title}/>
+                    </div>
+                    <div className="caption overlay">
+                      <h3>{project.title}</h3>
+                      <span>{project.category}</span>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        />
         <Route path={`${path}/wow-project`}>
           <div>
             wow project!
           </div>
         </Route>
-      </Switch>
+      </Router>
     </div>
   );
 };
