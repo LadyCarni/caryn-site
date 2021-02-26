678e16a414f901f44f9cf836964e181c7ef17cf1
Caryn Farvour <caryn.farvour@gmail.com>
2021-02-26 15:19:56 -0500

    updates analytics

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import ReactGA from 'react-ga';
-ReactGA.initialize('G-H0Z550MHEP');
+ReactGA.initialize('UA-190600893-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
