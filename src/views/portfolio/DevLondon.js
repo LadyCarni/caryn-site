e4c00e9374d605afdd211795b0e66caad5454be9
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-02 11:33:21 -0500

    adds description and images to dev london article

--- src/views/portfolio/DevLondon.js
+++ src/views/portfolio/DevLondon.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import { Link } from "react-router-dom";
 import devLondon from 'assets/portfolio/dev-london.jpg';
+import devLondonSlack from 'assets/portfolio/dev-london-slack.gif';
+
 
 class DevLondon extends Component {
   render() {
@@ -24,13 +25,21 @@ class DevLondon extends Component {
             <div className="section">
               <h3>Disciplines</h3>
               <ul className="tags">
-                <li>Front-end Development</li>
+                <li>Front-end Development (React)</li>
                 <li>Web Design</li>
               </ul>
             </div>
           </div>
           <div className="article">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid excepturi eaque at corrupti. Debitis reprehenderit similique officia neque hic odio, corrupti animi rerum minima, ex provident. Aut iste porro magni.</p>
+            <p className="external-link"><a href="https://devldn.ca/" target="_blank" rel="noreferrer">Dev London website</a></p>
+            <p className="external-link"><a href="https://github.com/DevLdn/devldn-website" target="_blank" rel="noreferrer">Dev London repo</a></p>
+            <p>Built with React, I designed and built a clean, simple site where we could highlight sponsors, cross-promote other peer groups, allow community members to submit ideas for talks, volunteer for events, and join the official Meetup page.</p>
+            <h3>Self-sufficient Slack</h3>
+            <p>To encourage the growth of the Dev London group, the site takes advantage of Slack's API to enable new members to join the Slack community without requiring a personal invite.</p>
+            <img src={devLondonSlack} alt="Dev London slack form"/>
+            <h3>Code cleanliness</h3>
+            <p>To maintain properly linted files automatically, the repo is equipped with a <a href="https://github.com/typicode/husky" target="_blank" rel="noreferrer">Husky</a> pre-commit git hook for running the <a href="https://github.com/prettier/prettier" target="_blank" rel="noreferrer">Prettier</a> SCSS and Javascript linters.</p>
+            
           </div>
         </div>
       </div>
