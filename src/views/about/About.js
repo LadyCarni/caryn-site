9ecaeec1d56376e9b7bdef92ee006735b988dc87
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-15 12:54:48 -0400

    adds photos and resume to about page

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowRight } from '@fortawesome/pro-light-svg-icons';
 import caryn1 from 'assets/caryn1.jpg';
+import caryn2 from 'assets/caryn5.jpg';
+import caryn3 from 'assets/caryn3.jpg';
+import caryn4 from 'assets/caryn4.jpg';
+import carynResume from 'assets/caryn-humphreys-resume-2021.pdf';
 
 const About = () => {
   return (
@@ -14,9 +17,22 @@ const About = () => {
         <div className="bio-content">
           <h1>Hi, I'm Caryn</h1>
           <p className="help">(pronounced kuh-<i>RIN</i>; like Corinne)</p>
-          <h2 className="title">I <span className="pop">design</span> experiences</h2>
-          <p>I'm a senior User Experience (UX) developer who is passionate about product design. I have been specializing in UX design for 9 years, and honing my front-end development and web design skills for the past 6 years. I've been leading cross-functional software teams through facilitation, coaching, and project management for the last 4 years. I have had training in accessibility for web, and have a formal education in business, marketing, and traditional art.</p>
-          <Link to="/about" className="arrow-link block">View Resume <FontAwesomeIcon icon={faLongArrowRight} /></Link>
+          <p>I'm a senior User Experience (UX) developer who is passionate about product design. I have been specializing in UX design for 9 years, and honing my front-end development and web design skills for the past 6 years. I've been leading cross-functional software teams through facilitation, coaching, and project management for 4 years. I have had training in accessibility for web, and have a formal education in business, marketing, and traditional art.</p>
+          <a href={carynResume} target="_blank" rel="noreferrer" className="arrow-link block">View Resume (PDF)<FontAwesomeIcon icon={faLongArrowRight} /></a>
+          <div className="gallery">
+            <div className="image">
+              <img src={caryn2} alt="Caryn Farvour in the wild" className="caption"/>
+              <span className="image-caption">Wireframing in the wild</span>
+            </div>
+            <div className="image">
+              <img src={caryn3} alt="Caryn Farvour speaking at Erie Day of Code" className="caption"/>
+              <span className="image-caption">Speaking at Erie Day of Code</span>
+            </div>
+            <div className="image">
+              <img src={caryn4} alt="Caryn Farvour and her bengal, Nyx" className="caption left"/>
+              <span className="image-caption">My bengal assistant, Nyx</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
