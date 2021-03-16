e894586b7b772ab4cc4b8a1cce337b6d7fea2bc7
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-16 11:50:03 -0400

    removes unused gallery

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowRight } from '@fortawesome/pro-light-svg-icons';
 import caryn1 from 'assets/caryn1.jpg';
-import caryn2 from 'assets/caryn5.jpg';
-import caryn3 from 'assets/caryn3.jpg';
-import caryn4 from 'assets/caryn4.jpg';
 import carynResume from 'assets/caryn-humphreys-resume-2021.pdf';
 
 const About = () => {
@@ -329,20 +326,6 @@ const About = () => {
           <p className="help">(pronounced kuh-<i>RIN</i>; like Corinne)</p>
           <p>I'm a senior User Experience (UX) developer who is passionate about product design. I have been specializing in UX design for 8 years, and honing my front-end development and web design skills for the past 7 years. I've been leading cross-functional software teams through facilitation, coaching, and project management for 4 years. I have had training in accessibility for web, and have a formal education in business, marketing, and traditional art.</p>
           <a href={carynResume} target="_blank" rel="noreferrer" className="arrow-link block">View Resume (PDF)<FontAwesomeIcon icon={faLongArrowRight} /></a>
-          {/* <div className="gallery">
-            <div className="image">
-              <img src={caryn2} alt="Caryn Farvour in the wild" className="caption"/>
-              <span className="image-caption">Wireframing in the wild</span>
-            </div>
-            <div className="image">
-              <img src={caryn3} alt="Caryn Farvour speaking at Erie Day of Code" className="caption"/>
-              <span className="image-caption">Speaking at Erie Day of Code</span>
-            </div>
-            <div className="image">
-              <img src={caryn4} alt="Caryn Farvour and her bengal, Nyx" className="caption left"/>
-              <span className="image-caption">My bengal assistant, Nyx</span>
-            </div>
-          </div> */}
         </div>
       </div>
       <div className="timeline">
