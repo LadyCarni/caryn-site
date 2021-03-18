c48fc8ec45e5c536f3e1576a9daf5029190b8097
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-18 12:57:35 -0400

    adds new sections to fongo piece

--- src/views/portfolio/FongoWorks.js
+++ src/views/portfolio/FongoWorks.js
@@ -6,6 +6,10 @@ import fongoWire1 from "assets/portfolio/fongo-wire1.jpg";
 import fongoSignIn from "assets/portfolio/fongo-signin.png";
 import fongoWire2 from "assets/portfolio/fongo-wire2.jpg";
 import fongoQuickstart from "assets/portfolio/fongo-quickstart.png";
+import fongoWire3 from "assets/portfolio/fongo-wire3.jpg";
+import fongoSetup from "assets/portfolio/fongo-setup.png";
+
+
 
 const FongoWorks = () => {
   return (
@@ -27,7 +31,7 @@ const FongoWorks = () => {
             <h3>Primary Goals</h3>
             <ul>
               <li>Replicating the familiarity of physical phone systems</li>
-              <li>Streamlining a complex set-up process</li>
+              <li>Streamlining a complex set up process</li>
               <li>Enabling new set ups in one session</li>
             </ul>
             <h2>The Results</h2>
@@ -48,16 +52,21 @@ const FongoWorks = () => {
                 <img src={fongoWire1} alt="Fongo Works splash page wireframe"/>
               </div>
             </div>
-            <img src={fongoSignIn} alt="Fongo Works splash page"/>
+          </div>
 
+          <div className="full-width">
+            <img src={fongoSignIn} alt="Fongo Works splash page"/>
+          </div>
+
+          <div className="article">
             <h3>Quickstart</h3>
             <p>Careful consideration of the copywriting throughout this part of the project was articulated even at the wireframe phase of the design. What the user would be setting up could quickly become an overwhelming task. Simplicity and clarity were a top priority. As we designed and developed the entire quickstart flow, we ran multiple usability tests to refine the steps, eliminate questions, and speed up the process to get the time to set up as short as possible. We were thrilled to get the time down to an average of 10 minutes even for non-technically savvy users.</p>
             <div className="flex columns">
               <div className="fit">
                 <p>Features:</p>
                 <ul>
-                  <li>The steps involved in the Quickstart process</li>
-                  <li>The (short!) time investment for the process</li>
+                  <li>The steps to be expected</li>
+                  <li>The (short!) time required</li>
                   <li>Clear data pre-requisites</li>
                 </ul>
               </div>
@@ -65,9 +74,29 @@ const FongoWorks = () => {
                 <img src={fongoWire2} alt="Fongo Works quickstart intro wireframe"/>
               </div>
             </div>
+          </div>
+          <div className="full-width">
             <img src={fongoQuickstart} alt="Fongo Works quickstart intro"/>
+          </div>
 
-
+          <div className="article">
+            <h3>New Account set up</h3>
+            <p>Many initial decisions are made during wireframing about structure, status, and design patterns that will be used. With this particular screen, after several iterations, the UI developed into a more comprehensive version of what was originally planned.</p>
+            <div className="flex columns">
+              <div className="fit">
+                <p>Features:</p>
+                <ul>
+                  <li>Simple, visual groups of forms</li>
+                  <li>Helpful copy where relevant</li>
+                </ul>
+              </div>
+              <div className="fill">
+                <img src={fongoWire3} alt="Fongo Works set up wireframe"/>
+              </div>
+            </div>
+          </div>
+          <div className="full-width">
+            <img src={fongoSetup} alt="Fongo Works set up"/>
           </div>
         </div>
       </div>
