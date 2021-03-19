dd33cb1a40a9aa554244e2a7adc36377f4f98058
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-19 13:31:47 -0400

    adds cover image for luna

--- src/views/portfolio/LunaNova.js
+++ src/views/portfolio/LunaNova.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PortfolioHeader from "components/portfolioHeader";
+import { PortfolioRoutes } from "./index";
+import lunaCover from "assets/portfolio/luna-cover.png";
 
 const LunaNova = () => {
   return (
     <>
-      
+      <PortfolioHeader
+        headerImage={lunaCover}
+        routes={PortfolioRoutes}
+        imageAlt="Luna Nova website"
+        headerTitle="Luna Nova"
+        headerSubtitle="An approachable dog breeder website"
+        headerDescription="Luna Nova is a Portuguese Water Dog breeder operating from a home-based business in Cambridge, Ontario"
+        headerDisciplines={["Web Design", "Front-end Development (React)"]}
+      />
+      <div className="feature feature-width">
+        <div className="content flex column">
+          <div className="article">
+            <p className="external-link"><a href="https://www.lunanovaportuguesewaterdogs.com/" target="_blank" rel="noreferrer">Luna Nova</a></p>
+            <h2>The Challenge</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nulla, aliquid, animi eligendi optio aliquam doloremque deleniti laboriosam quasi soluta nostrum ea nihil quia deserunt nisi inventore, architecto natus error.</p>
+            <h3>Primary Goals</h3>
+            <ul>
+              <li>thing</li>
+              <li>another thing</li>
+              <li>yet another</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
   )
 };
--- src/views/portfolio/index.js
+++ src/views/portfolio/index.js
@@ -4,13 +4,14 @@ import Portfolio from "./Portfolio";
 import KnakCaseStudy from './KnakCaseStudy';
 import Placeholder from './Placeholder';
 import FongoWorks from "./FongoWorks";
+import LunaNova from "./LunaNova";
 
 export const PortfolioRoutes = [
   { path: "/portfolio", name: "Portfolio", component: Portfolio, exact: true },
   { path: "/portfolio/connect-api", name: "Connect API", component: ConnectApi },
   { path: "/portfolio/dev-london", name: "Dev London", component: DevLondon },
   { path: "/portfolio/knak-ux-case-study", name: "Knak Case Study", component: KnakCaseStudy },
-  { path: "/portfolio/luna-nova", name: "Luna Nova", component: Placeholder },
+  { path: "/portfolio/luna-nova", name: "Luna Nova", component: LunaNova },
   { path: "/portfolio/fongo-works", name: "Fongo Works", component: FongoWorks },
   { path: "/portfolio/financial-app", name: "Financial App", component: Placeholder },
 ];
