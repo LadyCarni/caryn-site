98363ccda8fb06adaf5b0feb7b9dbc5bbc9df772
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-30 09:52:03 -0400

    adds content to lifestyle app

--- src/views/portfolio/Lifestyle.js
+++ src/views/portfolio/Lifestyle.js
@@ -24,17 +24,45 @@ const Lifestyle = () => {
         <div className="content flex column">
           <div className="article">
             <h2>The Challenge</h2>
-            <p>With so many competing breeders, my client needed to stand out and make her customers feel secure knowing they were adopting from a registered, safe, and loving home. Her website needed to be informative, transparent, and a showcase for the caliber of dogs she breeds. Many users would be non technically savvy, and would require an intuitive, smooth experience using and browsing the site.</p>
+            <p>The client my team was working with for this project was a startup that had created an innovative new product they were aiming to introduce to the market at CES that year. The product was essentially a beefed up video capture device. To support the product, they needed an app that would encourage the growth of a community of users around the videos produced by the product. The business proposition was unique enough that the design of the app needed to be intuitive and compelling to overcome the possible barrier to entry for the market.</p>
             <h3>Primary Goals</h3>
             <ul>
               <li>Familiar</li>
               <li>Intuitive to use</li>
+              <li>Intriguing and compelling</li>
             </ul>
 
             <h2>Design Features</h2>
+            <p>The heart of the app would be handling the videos the device captured. The user would need to be able to view and manage their videos as simply as possible.</p>
+
+            <h3>Video Editor</h3>
+            <p>While viewing a video page, the user could view the footage as well as the GPS location where the video took place. They would also be able to manage the data surrounding their videos. With this much control, the users would begin building a history of detailed video 'memories'.</p>
+          </div>
+          <div className="full-width">
             <img src={lifestyleCover} alt="Features of the app videos" className="shadow"/>
+          </div>
+
+          <div className="article">
+            <h3>Inline Editing</h3>
+            <p>Simple inline editing functionality was designed so that the user wouldn't need to navigate away from the page.</p>
+          </div>
+          <div className="full-width">
             <img src={lifestyleEdit} alt="Editing the title of a video" className="shadow"/>
+          </div>
+
+          <div className="article">
+            <h3>Privacy</h3>
+            <p>We knew from usability interviews early on that users would want the ability to customize the visibility options for videos they captured.</p>
+          </div>
+          <div className="full-width">
             <img src={lifestyleVisibility} alt="Editing the visibility settings" className="shadow"/>
+          </div>
+
+          <div className="article">
+            <h3>Video Tags</h3>
+            <p>Planning for scalability, we designed a tagging system that would allow the user to create customized organization for their video memories.</p>
+          </div>
+          <div className="full-width">
             <img src={lifestyleTags} alt="Viewing the tags of a video" className="shadow"/>
           </div>
         </div>
