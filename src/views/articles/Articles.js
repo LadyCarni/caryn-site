4813e9c8baad4edd8f0bba58b5859eb2e4ad6b8a
Caryn Farvour <caryn.farvour@gmail.com>
2021-03-30 12:52:16 -0400

    creates a feature header for articles

--- src/views/articles/Articles.js
+++ src/views/articles/Articles.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Articles = () => {
   return (
-    <div className="dark feature feature-width full-height">
-      <h2>Articles</h2>
-      <p>Coming soon for your reading pleasure!</p>
+    <div className="articles full-height">
+      <div className="article-features">
+        <div className="main-feature">
+          <div className="main-feature-content">
+            main feature
+          </div>
+        </div>
+        <div className="secondary-features">
+          <div className="secondary-feature-content">secondary feature 1</div>
+          <div className="secondary-feature-content">secondary feature 2</div>
+          <div className="secondary-feature-content">secondary feature 3</div>
+          <div className="secondary-feature-content">secondary feature 4</div>
+        </div>
+      </div>
+      
     </div>
   );
 }
