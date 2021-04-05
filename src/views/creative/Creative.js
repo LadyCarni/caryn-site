f0b7e8ffbb3e5509a8e82944f98835b4e59d9db9
Caryn Farvour <caryn.farvour@gmail.com>
2021-04-05 11:37:22 -0400

    creates placeholder for creativity view

--- src/views/creative/Creative.js
+++ src/views/creative/Creative.js
@@ -9,13 +9,22 @@ const Creative = () => {
           <img src={header} alt="Blueberry lemon cake"/>
         </div>
         <h1><span className="pop">Creativity</span> in the wild</h1>
-        <ul>
+        <h2>Coming soon!</h2>
+        {/* <ul>
           <li>Crafts</li>
           <li>Recipes</li>
           <li>DIY</li>
           <li>Photography</li>
-        </ul>
+        </ul> */}
       </div>
+      {/* <div className="creative-body">
+        <div className="creative-nav">
+
+        </div>
+        <div className="creative-thumbs">
+
+        </div>
+      </div> */}
     </div>
   )
 }
