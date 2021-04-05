6bbbc25acfba41be1035dedc26d4ca47f77fa2c3
Caryn Farvour <caryn.farvour@gmail.com>
2021-04-05 11:28:44 -0400

    adds header to creative view

--- src/views/creative/Creative.js
+++ src/views/creative/Creative.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import header from 'assets/creative/creative-header.jpg';
 
 const Creative = () => {
   return (
-    <div className="feature portfolio">
-      Creative portfolio
+    <div className="creative full-height">
+      <div className="creative-header">
+        <div className="header-image">
+          <img src={header} alt="Blueberry lemon cake"/>
+        </div>
+        <h1><span className="pop">Creativity</span> in the wild</h1>
+        <ul>
+          <li>Crafts</li>
+          <li>Recipes</li>
+          <li>DIY</li>
+          <li>Photography</li>
+        </ul>
+      </div>
     </div>
   )
 }
