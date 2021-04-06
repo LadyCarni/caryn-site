532eec4872232e4f75403050d8c7572b23e0afeb
Caryn Farvour <caryn.farvour@gmail.com>
2021-04-06 12:14:53 -0400

    creates masonry grid for sections of creative pages

--- src/views/creative/Placeholder.js
+++ src/views/creative/Placeholder.js
@@ -23,7 +23,70 @@ const Placeholder = () => {
             </ul>
           </div>
           <div className="creative-thumbs">
-            Project directory
+            <div className="item">
+              <p>1 Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            <div className="item span-2">
+              <p>2 Lorem ipsum dolor adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati sapiente sunt riores accusantium vitae reiciendis qui deserunt natus, minima.</p>
+            </div>
+            <div className="item">
+              <p>3 Lorem ipsum dolor</p>
+            </div>
+            <div className="item span-2">
+              <p>4 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis veniam adipisci commodi esse, rem mollitia doloribus velit neque.</p>
+            </div>
+            <div className="item">
+              <p>5 Lorem ipsum dolor</p>
+            </div>
+            <div className="item span-2-tall">
+              <p>6 Lorem ipsum dolor sit amet, ccimus, fuga itaque accusamus voluptate consequuntur, temporibus reiciendis! Dolore neque, molestias.</p>
+              <ul>
+                <li>Lorem</li>
+                <li>ipsum</li>
+                <li>dolor</li>
+                <li>sit</li>
+                <li>amet</li>
+                <li>consectetur</li>
+                <li>adipisicing</li>
+                <li>elit</li>
+                <li>Et</li>
+              </ul>
+            </div>
+            <div className="item">
+              <p>7 Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div className="item span-2-tall">
+              <p>8 Lorem ipsum dolor sit amet, ccimus, fuga itaque accusamus voluptate consequuntur, temporibus reiciendis! Dolore neque, molestias.</p>
+              <ul>
+                <li>Lorem</li>
+                <li>ipsum</li>
+                <li>dolor</li>
+                <li>sit</li>
+                <li>amet</li>
+                <li>consectetur</li>
+                <li>adipisicing</li>
+                <li>elit</li>
+                <li>Et</li>
+              </ul>
+            </div>
+            <div className="item span-3">
+              <p>9 Lorem ipsum dolor sit amet, lit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt eriores accusantium vitae reiciendis qui deserunt natus, minima.</p>
+            </div>
+            <div className="item">
+              <p>10 Lorem ipsum dolor sit amet, consecutetur admlg no scope</p>
+            </div>
+            <div className="item">
+              <p>11 Lorem ipsum dolor sit amet,</p>
+            </div>
+            <div className="item">
+              <p>12 Lorem ipsum dolor</p>
+            </div>
+            <div className="item">
+              <p>13 Lorem ipsum dolor sit amet,. Et odit, esse. Officiis, </p>
+            </div>
+            <div className="item">
+              <p>14 Lorem ipsum dolor sit</p>
+            </div>
           </div>
         </div>
       </div>
