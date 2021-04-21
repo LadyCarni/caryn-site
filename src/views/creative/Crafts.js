997b7e42c7dd1d3e9fab7c4816c37bb1831d49a6
Caryn Farvour <caryn.farvour@gmail.com>
2021-04-21 15:38:05 -0400

    creates initial placeholder crafts view with images

--- src/views/creative/Crafts.js
+++ src/views/creative/Crafts.js
@@ -1,11 +1,119 @@
 import React from "react";
 import header from 'assets/creative/craft-header.jpg';
 import CreativeHeader from 'components/creativeHeader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPalette } from "@fortawesome/pro-light-svg-icons";
 import Breadcrumb from 'components/breadcrumb';
 import { CreativeRoutes } from "./index";
-
+import stringArt from 'assets/creative/crafts/string-art6.jpg';
+import uvPainting from 'assets/creative/crafts/uv-4.jpg';
+import vinylElephant from 'assets/creative/crafts/vinyl-elephant.jpg';
+import mathMat from 'assets/creative/crafts/sewing-math-mat1.jpg';
+import brandedSwag from 'assets/creative/crafts/branded-swag.jpg';
+import dollhouseBricks from 'assets/creative/crafts/dollhouse-bricks.jpg';
+import tayloredShirts from 'assets/creative/crafts/taylored3.jpg';
+import roomVinyls from 'assets/creative/crafts/vinyl-room6.jpg';
+import bottleVinyls from 'assets/creative/crafts/vinyl-wine-label.jpg';
+import quiltStencil from 'assets/creative/crafts/quilt-stencil1.jpg';
+import vinylStudio from 'assets/creative/crafts/vinyl-studio.jpg';
+import blueQuilt from 'assets/creative/crafts/quilt-blue3.jpg';
+import tanQuilt from 'assets/creative/crafts/tan-quilt1.jpg';
 
 const Crafts = () => {
+  const allCrafts = [
+    { 
+      title: "String Art",
+      image: stringArt,
+      link: "#",
+      thumb: "item large-square soon",
+      id: 0,
+    },
+    { 
+      title: "UV Painting",
+      image: uvPainting,
+      link: "#",
+      thumb: "item soon",
+      id: 1,
+    },
+    { 
+      title: "Vinyl Elephant",
+      image: vinylElephant,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 2,
+    },
+    { 
+      title: "Sewing Math Mat",
+      image: mathMat,
+      link: "#",
+      thumb: "item soon",
+      id: 3,
+    },
+    { 
+      title: "Branded Swag",
+      image: brandedSwag,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 4,
+    },
+    { 
+      title: "Dollhouse Bricks",
+      image: dollhouseBricks,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 5,
+    },
+    { 
+      title: "Taylored Shirts",
+      image: tayloredShirts,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 6,
+    },
+    { 
+      title: "Vinyl Room Signs",
+      image: roomVinyls,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 7,
+    },
+    { 
+      title: "Quilt Stencil",
+      image: quiltStencil,
+      link: "#",
+      thumb: "item soon",
+      id: 8,
+    },
+    { 
+      title: "Vinyl Wine Label",
+      image: bottleVinyls,
+      link: "#",
+      thumb: "item soon",
+      id: 9,
+    },
+    { 
+      title: "Blue Quilt",
+      image: blueQuilt,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 10,
+    },
+    { 
+      title: "Vinyl Studio",
+      image: vinylStudio,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 11,
+    },
+    { 
+      title: "Tan Quilt",
+      image: tanQuilt,
+      link: "#",
+      thumb: "item rectangle soon",
+      id: 12,
+    },
+  ];
+
   return (
     <div className="creative full-height">
       <Breadcrumb routes={CreativeRoutes} className="creative-view" />
@@ -16,42 +124,17 @@ const Crafts = () => {
       <div className="creative-body-container">
         <div className="creative-body">
           <div className="creative-thumbs">
-            <div className="item large-square">
-              <p>String Art</p>
-            </div>
-            <div className="item">
-              <p>UV Painting</p>
-            </div>
-            <div className="item">
-              <p>Vinyl Elephant</p>
-            </div>
-            <div className="item rectangle">
-              <p>Branded Swag</p>
-            </div>
-            <div className="item">
-              <p>Sewing Math Mat</p>
-            </div>
-            <div className="item">
-              <p>Dollhouse Bricks</p>
-            </div>
-            <div className="item">
-              <p>Taylored Shirts</p>
-            </div>
-            <div className="item large-square">
-              <p>Vinyl Room Signs</p>
-            </div>
-            <div className="item">
-              <p>Vinyl Wine Label</p>
-            </div>
-            <div className="item">
-              <p>Quilt Stencil</p>
-            </div>
-            <div className="item rectangle">
-              <p>Blue Quilt</p>
-            </div>
-            <div className="item">
-              <p>Vinyl Studio</p>
-            </div>
+            {allCrafts.map(({id, title, image, link, thumb}) => (
+              <div className={thumb} key={id}>
+                <img src={image} alt={title}/>
+                <div className="photo-info">
+                  <a href={link}>
+                    <p>{title}</p>
+                    <span className="link-text">View craft</span><FontAwesomeIcon icon={faPalette} />
+                  </a>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
