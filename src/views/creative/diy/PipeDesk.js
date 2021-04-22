17f66ad67459f1741e3212bc0a5cc475259ca7f8
Caryn Farvour <caryn.farvour@gmail.com>
2021-04-22 17:45:43 -0400

    more media query hell

--- src/views/creative/diy/PipeDesk.js
+++ src/views/creative/diy/PipeDesk.js
@@ -19,6 +19,39 @@ import pipeDesk14 from 'assets/creative/diy/pipe-desk14.jpg';
 import pipeDesk15 from 'assets/creative/diy/pipe-desk15.jpg';
 import pipeDesk16 from 'assets/creative/diy/pipe-desk16.jpg';
 
+const MaterialsList = ( {media} ) => {
+  return (
+    <div className={`${media} materials`}>
+      <div className="diy-materials">
+        <h3>Materials for 46"x24" desk top</h3>
+        <ul>
+          <li>7.67 ft² of 3/4" wood</li>
+          <li>46 x 24" plywood</li>
+          <li>Foam brushes</li>
+          <li>Sandpaper</li>
+          <li>Stain</li>
+          <li>Polyurethane</li>
+          <li>(optional) epoxy</li>
+          <li>(optional) wood filler</li>
+        </ul>
+      </div>
+      <div className="diy-materials">
+        <h3>Materials for 3/4" plumbing pipe frame</h3>
+        <ul>
+          <li>3/4" flanges x 8</li>
+          <li>3/4" tees x 6</li>
+          <li>3/4" pipe at 38" x 1 (cross brace/foot rest)</li>
+          <li>3/4" pipe at 20.5" x 4 (top of the legs)</li>
+          <li>3/4" pipe at 13.5" x 2 (front of the side braces)</li>
+          <li>3/4" pipe at 4.5" x 6 (four for the bottom of the legs, two for the back of the side braces)</li>
+          <li>Steel wool (for 'sanding' the pipes and scrubbing off any paint or stamps)</li>
+          <li>Cleaning supplies (the pipes often come with a bit of oil on them you'll need to clean off)</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
+
 const PipeDesk = () => {
   return (
     <div className="creative full-height">
@@ -41,6 +74,8 @@ const PipeDesk = () => {
               </div>
             </div>
             <img src={pipeDesk} alt="pipe desk"/>
+
+            <MaterialsList media="mobile" />
 
             <div className="diy-instructions">
               <p><span className="instruction-step">Step 1. Cut the wood</span> Cut your hardwood floor lengths based on a pattern you like, and place on a base layer for your desk top. We used a Russian Birch plywood for the 3/4" tongue and groove hardwood flooring we found at <a href="https://www.timelessmaterials.com/" target="_blank" rel="noreferrer">Timeless Materials</a>. Take your time cutting and figuring out your pattern in this step.</p>
@@ -98,36 +133,9 @@ const PipeDesk = () => {
               <img src={pipeDesk15} alt="pipe desk finished"/>
               <img src={pipeDesk16} alt="pipe desk finished"/>
             </div>
+          </div>
 
-          </div>
-          <div className="materials">
-            <div className="diy-materials">
-              <h3>Materials for 46"x24" desk top</h3>
-              <ul>
-                <li>7.67 ft² of 3/4" wood</li>
-                <li>46 x 24" plywood</li>
-                <li>Foam brushes</li>
-                <li>Sandpaper</li>
-                <li>Stain</li>
-                <li>Polyurethane</li>
-                <li>(optional) epoxy</li>
-                <li>(optional) wood filler</li>
-              </ul>
-            </div>
-            <div className="diy-materials">
-              <h3>Materials for 3/4" plumbing pipe frame</h3>
-              <ul>
-                <li>3/4" flanges x 8</li>
-                <li>3/4" tees x 6</li>
-                <li>3/4" pipe at 38" x 1 (cross brace/foot rest)</li>
-                <li>3/4" pipe at 20.5" x 4 (top of the legs)</li>
-                <li>3/4" pipe at 13.5" x 2 (front of the side braces)</li>
-                <li>3/4" pipe at 4.5" x 6 (four for the bottom of the legs, two for the back of the side braces)</li>
-                <li>Steel wool (for 'sanding' the pipes and scrubbing off any paint or stamps)</li>
-                <li>Cleaning supplies (the pipes often come with a bit of oil on them you'll need to clean off)</li>
-              </ul>
-            </div>
-          </div>
+          <MaterialsList media="desktop" />
         </div>
       </div>
     </div>
