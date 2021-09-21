ddfd6b365d0fbaf4a2c34ed4522116adbef13363
Caryn Farvour <caryn.farvour@gmail.com>
2021-09-21 08:30:05 -0400

    begins writing content for financial app

--- src/views/portfolio/FinancialApp.js
+++ src/views/portfolio/FinancialApp.js
@@ -6,6 +6,7 @@ import financialCart from "assets/portfolio/financial-cart.png";
 import financialCheckout from "assets/portfolio/financial-checkout.jpg";
 import financialMarketPrice from "assets/portfolio/financial-market-price.png";
 import financialPayment from "assets/portfolio/financial-payment-options.png";
+import financialVault from "assets/portfolio/financial-vault.png";
 
 const Financial = () => {
   return (
@@ -31,12 +32,26 @@ const Financial = () => {
               <li>flexibility</li>
               <li>helpful, thorough (market, taxes, etc.)</li>
             </ul>
-            <h2>The Results</h2>
-            <p>Results...</p>
+            <h2>Research & Planning</h2>
+            <p>Domain knowledge; market price, security and insurance, storing precious metals user needs and behavior research; what they need to make the decision to buy or sell precious metals</p>
+            <h2>Design Features</h2>
+            <p>Given the complexity of the industry, and the need for a seamless, efficient user experience, each feature for the app needed to be designed with transparency and simplicity in mind.</p>
+            <h3>Vault Location</h3>
+            <p>Customers would have the option to either have their precious metals shipped to themselves or stored in a vault. To give the customer a sense of transparency and and security, the checkout would show the specific location of the vault as well as the logo of the reputed, secure provider of the vault (hidden in this example for privacy purposes).</p>
+            <img src={financialVault} alt="Financial app vault location"/>
+
+            <h3>Market Price Lock</h3>
+            <p>Providing adequate information and context around the purchase of precious metals was an important part of the control we wanted to give the customer during their experience. One way we provided this control was by showing the market price at the time the customer placed the item in their cart, and locking that price for a limited time to allow them to complete their purchase.</p>
+            <img src={financialMarketPrice} alt="Financial app market price lock"/>
+
+            <h3>Order Summary</h3>
             <img src={financialCart} alt="Financial app shopping cart"/>
-            <img src={financialCheckout} alt="Financial app shopping cart"/>
-            <img src={financialMarketPrice} alt="Financial app shopping cart"/>
+
+            <h3>Payment</h3>
             <img src={financialPayment} alt="Financial app shopping cart"/>
+
+            <h3>Checkout Experience</h3>
+            <img src={financialCheckout} alt="Financial app shopping cart"/>
           </div>
         </div>
       </div>
