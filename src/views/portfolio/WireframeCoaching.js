d82bdc7f3b7343c8f97bd868e4929f187b1e3e62
Caryn Farvour <caryn.farvour@gmail.com>
2021-10-16 10:42:38 -0400

    continues wireframe coaching content

--- src/views/portfolio/WireframeCoaching.js
+++ src/views/portfolio/WireframeCoaching.js
@@ -2,6 +2,7 @@ import React from "react";
 import PortfolioHeader from "components/portfolioHeader";
 import { PortfolioRoutes } from "./index";
 import wireframeHeader from "assets/caryn5.jpg";
+import wireframeTools from "assets/portfolio/wireframe-tools.jpg";
 import usePrism from "hooks/usePrism";
 
 const WireframeCoaching = () => {
@@ -26,6 +27,7 @@ const WireframeCoaching = () => {
             <p>Wireframing is one of those simple but great skills to have on any software team, regardless of role or responsibility. Over the years I've run my wireframing workshop for small teams in an intimate setting, large teams over the course of an afternoon, at Universities for students studying HCI, and at various software user groups.</p>
             
             <h2>Why does it matter?</h2>
+            <h3>Communication, collaboration, efficient planning</h3>
             <p>Wireframing is the part of the UX process where you begin to visualize the landscape of the interface you’ll be designing. Being able to quickly sketch out ideas will help you come up with ideas as well as communicate those ideas more effectively to other people.</p>
 
             <p>Wireframing answers key questions before a design begins:</p>
@@ -39,6 +41,7 @@ const WireframeCoaching = () => {
             </ul>
 
             <h2>Getting started</h2>
+            <h3>Mindset and clear language</h3>
             <p>The most popular response to the idea of wireframing is: <i>"But I can't draw!"</i> To which I always respond: <i>"You don't need to be able to draw to wireframe."</i>.</p>
 
             <p>To get participants comfortable with UX as a whole, I start each workshop going through some basic UX terminology and jargon UX professionals like to use. This is intended to break down some of the barriers around UX that makes it seem magical and out of reach.</p>
@@ -55,7 +58,23 @@ const WireframeCoaching = () => {
             <p><b>Copywriting:</b> Writing the content using intentional language, tone, and sentence structure, including <i>microcopy</i>; the wording used in buttons, labels, titles, etc.</p>
 
             <h2>What tools are needed?</h2>
-            <p>For each workshop, I provide all the participants with everything they need: sticky notes, graph paper, pens, and markers. I explain that if you use a pencil for temporary things like wireframing you’ll be tempted to erase mistakes. Using a pen forces you to get used to how disposable these are intended to be.</p>
+            <h3>Pen and paper</h3>
+
+            <div className="flex columns">
+              <div className="fill">
+                <img src={wireframeTools} alt="pen and paper" className="shadow"/>
+              </div>
+              <div className="fill">
+                <p>For each workshop, participants are provided with everything they need: sticky notes, graph paper, pens, and markers.</p>
+                <p>I explain that if you use a pencil for temporary things like wireframing you’ll be tempted to erase mistakes. Using a pen forces you to get used to how disposable these are intended to be.</p>
+              </div>
+            </div>
+
+            <h2>Wireframing basics</h2>
+            <h3>Boxes and lines</h3>
+            <p>At its core, wireframing can be done with simple boxes and lines. It's for this reason that graph paper can be such a help.</p>
+
+            
           </div>
         </div>
       </div>
